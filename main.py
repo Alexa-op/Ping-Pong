b43f464ab86dea8f015ca6eb815d8f41e6a7d933
Alexa-op <83080848+Alexa-op@users.noreply.github.com>
2021-05-07 19:30:43 +0500

    1.1
    add ball and collide from horizontal walls and rokets

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from pygame import *
 from random import randint
 from time import time as timer
 
+h = 600
+w = 700
+speed_x = 3
+speed_y = 3
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
         super().__init__()
@@ -20,12 +25,20 @@ class Player(GameSprite):
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         elif keys[K_DOWN] and self.rect.y < 545:
-            self.rect.x += self.speed
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        elif keys[K_s] and self.rect.y < 545:
+            self.rect.y += self.speed
 
-window = display.set_mode((700, 500))
+window = display.set_mode((w, h))
 display.set_caption("Ping-Pong")
-bg = transform.scale(image.load("bg.jpeg"),(700, 550))
-rk1 = Player("rk1.png", 20, 275, 5, 50, 50)
+bg = transform.scale(image.load("bg.jpeg"),(w, h))
+rk1 = Player("rk1.png", 20, 275, 7, 100, 100)
+rk2 = Player("rk2.png", 600, 275, 7, 100, 100)
+ball = Player("ball.png", 350, 275, 3, 50, 50)
 
 clock = time.Clock()
 
@@ -38,8 +51,25 @@ while game == True:
             game = False
 
     if not finish:
-        window.blit(bg,(0, -10))
-        display.update()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y > h-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(rk1, ball) or sprite.collide_rect(rk2, ball):
+            speed_x *= -1
+
+
+        window.blit(bg,(0, 0))
+
+        ball.reset()
+        rk1.reset()
+        rk2.reset()
+
         rk1.update_l()
+        rk2.update_r()
+
+        display.update()
 
     clock.tick(30)
